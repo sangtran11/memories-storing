87fa4a7ec8f475bd861ccdf01d44ff9905d500bf
SangTran <sang.tran@adurolife.com>
2022-04-03 23:48:58 +0700

    create server structure, model, controllers and routes

--- server/index.js
+++ server/index.js
@@ -2,3 +2,24 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://memories_db_dev:admin123@cluster0.p5lcd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
