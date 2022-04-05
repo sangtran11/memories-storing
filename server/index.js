1189bf34a4f0ca2e343440599c3b9ef54083f571
SangTran <sang.tran@adurolife.com>
2022-04-05 22:31:13 +0700

    fix cors errors

--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/posts", postRoutes);
 const CONNECTION_URL =
   "mongodb+srv://memories_db_dev:admin123@cluster0.p5lcd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
