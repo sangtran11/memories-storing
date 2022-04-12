619cc640ffe09de85c8a14c08bf8e5734eec6735
SangTran <sang.tran@adurolife.com>
2022-04-12 16:44:37 +0700

    create actionsType constants file, implement UI for mobile

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,17 +1,19 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from "../constants/actionTypes";
+
 export default (posts = [], action) => {
   switch (action.type) {
-    case "FETCH_ALL":
+    case FETCH_ALL:
       return action.payload;
-    case "CREATE":
+    case CREATE:
       return [...posts, action.payload];
-    case "UPDATE":
+    case UPDATE:
       const index = posts.findIndex(p => p._id === action.payload._id);
       if (index === -1) {
         return posts;
       }
       posts[index] = action.payload;
       return [...posts];
-    case "DELETE":
+    case DELETE:
       return posts.filter((post) => post._id !== action.payload);
     default:
       return posts;
